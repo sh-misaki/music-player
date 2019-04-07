122744ac9ae7b039cc7266d7251a446a099776dd
sh-misaki <s.misaki.0000@gmail.com>
2019-04-07 12:49:59 +0900

    hash取得

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import Button from "../components/button";
+import axios from "axios";
+import cookies from "next-cookies";
 
 interface IProps {
   test: string;
@@ -14,10 +16,18 @@ export default class TopContainer extends React.Component<IProps, IState> {
     hash: "",
   };
 
-  private componentDidMount() {
+  private async componentDidMount() {
     const hashString = window.location.hash.split("&")[0].split("=")[1];
     this.setState({
       hash: hashString,
+    });
+
+    const res = await axios({
+      headers: {
+        Authorization: `Bearer ${hashString}`,
+      },
+      method: "get",
+      url: "https://api.spotify.com/v1/me",
     });
   }
 
@@ -26,7 +36,6 @@ export default class TopContainer extends React.Component<IProps, IState> {
     const clientId = "a1bd0a2ad2ce41edb109fdcc95206aea";
     const redirectUri = "http:%2F%2Flocalhost:3000%2F";
     const scope = "user-read-private%20user-read-email";
-    const { hash } = this.state;
 
     return (
       <div>
@@ -35,7 +44,6 @@ export default class TopContainer extends React.Component<IProps, IState> {
         }>
           <Button label="ログイン"/>
         </a>
-        hash: { hash }
       </div>
     );
   }
