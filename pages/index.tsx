d0cca282e999fc6995d50f2561d0cd19560d8534
sh-misaki <s.misaki.0000@gmail.com>
2019-04-08 21:53:52 +0900

    introduce styled-jsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,27 +7,48 @@ interface IProps {
   test: string;
 }
 
+interface IMusicItem {
+  context: {};
+  played_at: string;
+  track: {};
+}
+
 interface IState {
-  hash: string;
+  token: string;
+  items: IMusicItem;
 }
 
 export default class TopContainer extends React.Component<IProps, IState> {
-  public state: IState = {
-    hash: "",
-  };
+  private static async getInitialProps(ctx) {
+    const { token } = cookies(ctx);
+    return { token };
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      token: props.token,
+      items: [],
+    };
+  }
 
   private async componentDidMount() {
-    const hashString = window.location.hash.split("&")[0].split("=")[1];
-    this.setState({
-      hash: hashString,
-    });
-
+    let { token } = this.state;
+    token = token || window.location.hash.split("&")[0].split("=")[1];
     const res = await axios({
       headers: {
-        Authorization: `Bearer ${hashString}`,
+        Authorization: `Bearer ${token}`,
       },
       method: "get",
-      url: "https://api.spotify.com/v1/me",
+      url: "https://api.spotify.com/v1/me/player/recently-played",
+    });
+
+    document.cookie = `token=${token}`;
+
+    this.setState({
+      token: token,
+      items: res.data.items,
     });
   }
 
@@ -35,15 +56,27 @@ export default class TopContainer extends React.Component<IProps, IState> {
     const loginUri = "https://accounts.spotify.com/authorize";
     const clientId = "a1bd0a2ad2ce41edb109fdcc95206aea";
     const redirectUri = "http:%2F%2Flocalhost:3000%2F";
-    const scope = "user-read-private%20user-read-email";
+    const scope = "user-read-recently-played%20playlist-read-private";
+    const { items } = this.state;
 
     return (
       <div>
-        <a href={
-          `${loginUri}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=token&state=123`
-        }>
-          <Button label="ログイン"/>
-        </a>
+        {
+          false && (
+            <a href={
+              `${loginUri}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=token&state=123`
+            }>
+              <Button label="ログイン"/>
+            </a>
+          )
+        }
+        <div>
+          {
+            items.length && (
+              <img src={items[0].track.album.images[0].url}/>
+            )
+          }
+        </div>
       </div>
     );
   }
