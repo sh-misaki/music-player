421c7605a030a420e19ace237a0e6679e9ec0314
sh-misaki <s.misaki.0000@gmail.com>
2019-04-10 23:02:27 +0900

    スタイル周りの設定

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,83 +1,78 @@
 import * as React from "react";
-import Button from "../components/atoms/Button";
-import axios from "axios";
-import cookies from "next-cookies";
+
+import { CurryList } from "../components/organisms/CurryList";
+import { MainTitle, MainContent } from "../styled/Page";
 
 interface IProps {
-  test: string;
+  curries: ICurry[];
 }
 
-interface IMusicItem {
-  context: {};
-  played_at: string;
-  track: {};
+interface ICurry {
+  id: number;
+  name: string;
+  imageUrl: string;
 }
 
-interface IState {
-  token: string;
-  items: IMusicItem;
-}
+export default class BlogsPage extends React.Component<IProps> {
+  protected static async getInitialProps() {
+    try {
+      // const response = await fetch('https://??????.???/curries/india');
+      // const json = await response.json();
 
-export default class TopContainer extends React.Component<IProps, IState> {
-  private static async getInitialProps(ctx) {
-    const { token } = cookies(ctx);
-    return { token };
-  }
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      token: props.token,
-      items: [],
-    };
-  }
-
-  private async componentDidMount() {
-    let { token } = this.state;
-    token = token || window.location.hash.split("&")[0].split("=")[1];
-    const res = await axios({
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      method: "get",
-      url: "https://api.spotify.com/v1/me/player/recently-played",
-    });
-
-    document.cookie = `token=${token}`;
-
-    this.setState({
-      token: token,
-      items: res.data.items,
-    });
-  }
-
-  private render() {
-    const loginUri = "https://accounts.spotify.com/authorize";
-    const clientId = "a1bd0a2ad2ce41edb109fdcc95206aea";
-    const redirectUri = "http:%2F%2Flocalhost:3000%2F";
-    const scope = "user-read-recently-played%20playlist-read-private";
-    const { items } = this.state;
-
-    return (
-      <div>
+      // 通常では上記のように外部APIサーバーに対してデータを取得しにいきますが、今回は簡潔に済ますために
+      // static async getInitialProps() で直接データを returnすることにします。
+      // 下記のデータがAPIサーバーから返ってくると想定して、進めます。
+      // 画像はpixabay様の著作権フリー・帰属表示不要の画像を使っています。
+      // https://pixabay.com/ja/
+      const json: ICurry[] = [
         {
-          true && (
-            <a href={
-              `${loginUri}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=token&state=123`
-            }>
-              <Button label="ログイン"/>
-            </a>
-          )
+          id: 1,
+          name: "Curry1",
+          imageUrl: "/static/curry1.jpg"
+        },
+        {
+          id: 2,
+          name: "Curry2",
+          imageUrl: "/static/curry2.jpg"
+        },
+        {
+          id: 3,
+          name: "Curry3",
+          imageUrl: "/static/curry3.jpg"
+        },
+        {
+          id: 4,
+          name: "Curry4",
+          imageUrl: "/static/curry4.jpg"
+        },
+        {
+          id: 5,
+          name: "Curry5",
+          imageUrl: "/static/curry5.jpg"
+        },
+        {
+          id: 6,
+          name: "Curry6",
+          imageUrl: "/static/curry6.jpg"
         }
-        <div>
-          {
-            items.length && (
-              <img src={items[0].track.album.images[0].url}/>
-            )
-          }
-        </div>
-      </div>
+      ];
+
+      return {
+        curries: json
+      };
+    } catch (e) {
+      return {
+        curries: []
+      };
+    }
+  }
+
+  public render() {
+    return (
+      <MainContent>
+        <MainTitle>Indian Curries</MainTitle>
+        <CurryList curries={this.props.curries} />
+      </MainContent>
     );
   }
 }
