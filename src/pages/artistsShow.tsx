dc4cab7b9a79753864b9d4343d0bc625b94ac126
sh-misaki <s.misaki.0000@gmail.com>
2019-04-13 21:15:25 +0900

    詳細ページ

--- src/pages/artistsShow.tsx
+++ src/pages/artistsShow.tsx
@@ -1,11 +1,50 @@
 import * as React from "react";
+import axios from "axios";
+import cookies from "next-cookies";
 
-export default class BlogsPage extends React.Component {
+import Top from "~/components/containers";
+import Main from "~/components/templates/Main";
+
+interface ITopPage {
+  artist: object;
+  albums: object;
+  topTracks: object;
+}
+
+export default class TopPage extends React.Component<ITopPage> {
+  protected static async getInitialProps({ ctx }: any) {
+    const { token } = cookies(ctx);
+    const { id } = ctx.query;
+
+    if (!token) {return {}; }
+
+    const res = await axios.all([
+      `${id}`,
+      `${id}/albums`,
+      `${id}/top-tracks?country=jp`,
+    ].map((path: string) => {
+      return axios.create({
+        headers: { Authorization: `Bearer ${token}`, },
+      }).get(`https://api.spotify.com/v1/artists/${path}`);
+    }));
+
+    return {
+      artist: res[0].data,
+      albums: res[1].data,
+      topTracks: res[2].data.tracks,
+    };
+  }
+
   public render() {
+
     return (
-      <div>
-        artists show
-      </div>
+      <Main>
+        <Top
+          artist={this.props.artist}
+          albums={this.props.albums}
+          topTracks={this.props.topTracks}
+        />
+      </Main>
     );
   }
 }
