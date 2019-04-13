62fdf5f7cbc9415634477c028b6f803a0aa575b9
sh-misaki <s.misaki.0000@gmail.com>
2019-04-13 13:48:06 +0900

    トップページの作り方を変更

--- src/components/containers/index.tsx
+++ src/components/containers/index.tsx
@@ -1,3 +1,4 @@
+import React, { Component } from "react";
 import Link from "next/link";
 import { connect } from "react-redux";
 
@@ -5,38 +6,41 @@ import Counter from "~/components/organisms/counter";
 import Clock from "~/components/organisms/clock";
 
 export interface IPageProps {
-  error?: Error;
-  lastUpdate: string | number;
-  light?: boolean;
+  title: string;
   linkTo: string;
   NavigateTo: string;
-  title: string;
+  lastUpdate: string | number;
+  error?: Error;
+  light?: boolean;
 }
 
-function Page({
-  error,
-  lastUpdate,
-  light,
-  linkTo,
-  NavigateTo,
-  title,
-}: IPageProps) {
-  return (
-    <div>
-      <h1>{title}</h1>
-      <Clock
-        lastUpdate={lastUpdate}
-        light={light}
-      />
-      <Counter />
-      <nav>
-        <Link href={linkTo}>
-          <a>Navigate: {NavigateTo}</a>
-        </Link>
-      </nav>
-      {error && <p style={{ color: "red" }}>Error: {error.message}</p>}
-    </div>
-  );
+class Top extends Component<IPageProps> {
+  public render() {
+    const {
+      error,
+      lastUpdate,
+      light,
+      linkTo,
+      NavigateTo,
+      title,
+    } = this.props;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <Clock
+          lastUpdate={lastUpdate}
+          light={light}
+        />
+        <Counter />
+        <nav>
+          <Link href={linkTo}>
+            <a>Navigate: {NavigateTo}</a>
+          </Link>
+        </nav>
+        {error && <p style={{ color: "red" }}>Error: {error.message}</p>}
+      </div>
+    );
+  }
 }
 
-export default connect((state) => state)(Page);
+export default connect((state) => state)(Top);
