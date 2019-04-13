26e2b949d8e4bc5c405f9b235e71a6917c4ae9fa
sh-misaki <s.misaki.0000@gmail.com>
2019-04-13 16:00:32 +0900

    normalize.css:

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,7 @@ import Document, {
   NextDocumentContext
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import normalize from 'normalize.css/normalize.css';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -28,6 +29,7 @@ export default class MyDocument extends Document {
       <html lang="ja">
         <Head>
           {this.props.styleTags}
+          <style global>{normalize}</style>
         </Head>
         <body>
           <Main/>
